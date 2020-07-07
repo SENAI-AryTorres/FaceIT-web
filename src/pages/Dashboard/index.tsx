0e6325e3c678219a81efe66cab5bf1b1021941db
Matheus Moraes <matheussilva201654@gmail.com>
2020-07-07 10:27:55 -0300

    Commit pro noob

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,29 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Card, Grid, Button } from '@material-ui/core';
-import Icon from '@material-ui/core/Icon';
+import React from 'react';
+import { Grid } from '@material-ui/core';
 import { Container, Content, AnimationContainer } from './styles';
-import api from '../../services/api';
 
-interface PropostaItem {
-  descricao: string;
-  idProposta: string;
-}
+
 
 const Dashboard: React.FC = () => {
-  const token = localStorage.getItem('FaceIT:token');
-  const [propostaRetorno, setPropostas] = useState<PropostaItem[]>([]);
+ 
 
-  useEffect(() => {
-    const config = {
-      headers: { Authorization: `Bearer ${token}` },
-    };
-    // Carrega os dados do drop down
-    api.get(`/proposta`, config).then((res) => {
-      const proposta: PropostaItem[] = res.data;
-      setPropostas(proposta);
-    });
-  }, [token]);
-
+  
   return (
     <Container>
       <Content>
